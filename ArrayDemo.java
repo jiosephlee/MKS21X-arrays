84fa151090dd5ffa0ac0f5a25d064871e9c12b90
Joseph Lee <jlee01@stuy.edu>
2018-09-28 00:19:01 -0400

    finished code and tested it!

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -9,9 +9,23 @@ public class ArrayDemo{
         {0,1,2,3,0}, {3,4,5},{0,6,7,8}
       };
 
+      System.out.println("Test1");
       printArray(array);
+
+      System.out.println("Test2");
       printArray(secondArray);
+
+      System.out.println("Test3");
       System.out.println(countZeros2D(secondArray));
+
+      System.out.println("Test4");
+      fill2DCopy(secondArray);
+
+      System.out.println("Test4");
+      fill2D(secondArray);
+
+      System.out.println("Test5");
+      fill2DCopy(secondArray);
     }
 
     public static String toString(int[] ary){
@@ -50,12 +64,35 @@ public class ArrayDemo{
       return count;
     }
     public static void fill2D(int[][] vals){
-
+      for (int x = 0; x < vals.length; x++){
+        for (int y = 0; y < vals[x].length; y++){
+          if (x==y){
+            vals[x][y]=3;
+          }
+          else {
+            vals[x][y]=1;
+          }
+        }
+      }
+      printArray(vals);
     }
 
     public static int[][] fill2DCopy(int[][] vals){
-      int[][] array = new int[2][1];
+      int[][] array = new int[vals.length][];
+      for (int x = 0; x < vals.length; x++){
+        array[x]= new int[vals[x].length];
+      }
+      for (int x = 0; x < vals.length; x++){
+        for (int y = 0; y < vals[x].length; y++){
+          if (vals[x][y]<0){
+            array[x][y]=3;
+          }
+          else {
+            array[x][y]=1;
+          }
+        }
+      }
+      printArray(array);
       return array;
-
     }
 }
