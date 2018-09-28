c314c84790df3f3b7b80e9b5cc1baaa0256ba1a7
Joseph Lee <jlee01@stuy.edu>
2018-09-27 20:47:45 -0400

    created the structures for the methods

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+public class ArrayDemo{
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printArray(int[] ary){
+
+    }
+
+    public static void printArray(int[][] ary){
+
+    }
+
+    public static void fill2D(int[][] vals){
+
+    }
+
+    public static int[][] fill2D(int[][] vals){
+      
+    }
+}
