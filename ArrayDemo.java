61e9ee83a1cc4b9c33099b78388d11c2f40fd6d9
Joseph Lee <jlee01@stuy.edu>
2018-09-27 21:50:46 -0400

    wrote some code and tested it

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,9 +2,23 @@ public class ArrayDemo{
 
     public static void main(String[] args) {
 
+      int[] array = {
+        0,1,2,3,4,5,6,7,8,9,10
+      };
+      int[][] secondArray = {
+        {1,2,3}, {3,4,5},{6,7,8}
+      };
+
+      printArray(array);
     }
 
     public static void printArray(int[] ary){
+      String output = "{" + ary[0];
+      for (int x = 1; x < ary.length; x++){
+        output += "," + ary[x];
+      }
+      output+= "}";
+      System.out.println(output);
 
     }
 
@@ -16,7 +30,9 @@ public class ArrayDemo{
 
     }
 
-    public static int[][] fill2D(int[][] vals){
-      
+    public static int[][] fill2DCopy(int[][] vals){
+      int[][] array = new int[2][1];
+      return array;
+
     }
 }
